5f23904b7c00ec01386e2e2db3dae61c33c107a6
Martin Boksa <martin.boksa@gmail.com>
2021-04-09 15:55:23 +0200

    release action

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('This is Script js')
+const fs = require('fs');
+
+if (process.argv.length !== 3) {
+    throw new Error("version is missing")
+}
+
+const version = process.argv[2]
+const fileName = './package.json';
+const file = require(fileName);
+file.version = version
+
+fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log('writing version ' + version + ' to ' + fileName);
+});
